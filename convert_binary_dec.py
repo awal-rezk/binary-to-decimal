8246f887c089091de588f9755f39888dd66eb1ac
awal-rezk <80980181+awal-rezk@users.noreply.github.com>
2021-03-19 13:12:21 -0500

    Added convert_binary_dec.py

---
+++ convert_binary_dec.py
@@ -0,0 +1,18 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+def calc_decimal(binary):
+    binary_string = str(binary) #convert binary number to string
+    number_of_bits = len(binary_string)
+    decimal_equivalent = 0 #holds decimal equivalent of binary number
+    index = number_of_bits-1 #bit position in binary number
+    for i in binary_string:
+        if i != "0":#first convert individual character to integer to compare it to 0. Then, 0 x 2^any power = 0
+            temp = pow(2,index)
+            decimal_equivalent += temp
+        index -=1 #decrement the bit position --> moving to the right in the binary number
+    decimal_equivalent_string = str(decimal_equivalent)
+    print("This is the corresponding decimal value: " + decimal_equivalent_string)
+print("Please enter a binary number, with no leading zeroes (most significant bit).")
+calc_decimal(110)
+
+#compile on https://www.programiz.com/python-programming/online-compiler/
